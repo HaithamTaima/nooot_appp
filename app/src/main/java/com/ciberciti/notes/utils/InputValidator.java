8d27db9516f3b7c7e81e72d13136e5aa636a9cdc
HaithamTaima <haithamsrk@gmail.com>
2022-06-10 20:42:59 +0300

    Initial commit

---
+++ app/src/main/java/com/ciberciti/notes/utils/InputValidator.java
@@ -0,0 +1,34 @@
+package com.ciberciti.notes.utils;
+
+import java.util.regex.Pattern;
+
+public class InputValidator {
+    public boolean validateIndianMobileNumber(String number) {
+        if (number == null) return false;
+        Pattern pattern = Pattern.compile("^\\d{10}$");
+        return pattern.matcher(number).matches();
+    }
+
+    public boolean validateEmailAddress(String email) {
+        if (email == null) return false;
+        Pattern pattern = Pattern.compile(email);
+        return pattern.matcher(email).matches();
+    }
+
+    public boolean validatePassword(String password, String name) {
+        if (password.length() < 8) return false;
+        if (password.length() > 15) return false;
+        if (password.toLowerCase().contains(name.toLowerCase())) return false;
+        if (Character.isUpperCase(password.charAt(0))) return false;
+        int upperCaseCount = 0, digitCount = 0, specialCharCount = 0;
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) upperCaseCount++;
+            if (Character.isDigit(c)) digitCount++;
+            if (Character.isLetterOrDigit(c)) specialCharCount++;
+        }
+        if (upperCaseCount < 2) return false;
+        if (digitCount < 2) return false;
+        return specialCharCount >= 1;
+
+    }
+}
